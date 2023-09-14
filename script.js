ffc979040282d77ba0f2e17d117440143d1d9cc6
YeetusDilitus <theyeetusdilitus69@gmail.com>
2023-09-14 23:31:16 +0200

    Finished Hero Page
    Added slide in slide out animation to hamburger dropdown menu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links")
   const icon = document.querySelector(".hamburger-icon")
+  const nav = document.querySelector(".nav-bar")
 
   menu.classList.toggle("open")
   icon.classList.toggle("open")
+  nav.classList.toggle("open")
 }
